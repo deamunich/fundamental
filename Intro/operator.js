646e68576a12b652a589bfe763a298da95ce0ab8
deamunich <deamunich@gmail.com>
2024-08-29 20:28:12 +0700

    Updated operator.js

--- Intro/operator.js
+++ Intro/operator.js
@@ -21,3 +21,30 @@ console.log("20" - "40");
 console.log("1" + 1 + 1);
 
 console.log(null + 0);
+
+//Modify in place
+let n = 10;
+n = n * 2;
+n *= 2; // This is the short version to multiply `n` by 2
+n += 15; // This is the short version to add `n` by 2
+console.log("Modify in place: " + n);
+
+// Increment and decrement operators - only applicable for addition & subtraction
+let count = 1;
+count += 1;
+count++ // increment
+console.log("Increment: " + count);
+count-- // decrement
+console.log("Decrement: " + count);
+
+// postFix & preFix operators
+let preCounter = 1;
+console.log("preCounter: " + ++preCounter); // can immediately get the final result
+
+let postCounter = 1;
+console.log("postCounter: " + postCounter++); // cannot get the final result, print the previous value but it actually runs the code
+console.log(postCounter);
+
+// Comparison operators
+console.log(5 == "5");
+console.log(5 === "5");
